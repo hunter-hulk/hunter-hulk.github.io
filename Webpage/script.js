6cb82166180ab12a7591266bbf27b0bfcd1d5490
Samuel hunt <samuelhuntnz@gmail.com>
2020-08-18 12:58:40 +1200

    added comments and slideshow

--- Webpage/script.js
+++ Webpage/script.js
@@ -1,33 +1,48 @@
  // get all the lightbox elements and convert them to an array
-const lightboxElements = Array.from(document.querySelectorAll('[data-lightbox-src]'));
+ const lightboxElements = Array.from(document.querySelectorAll('[data-lightbox-src]'));
 
-// on each lightbox element
-lightboxElements.forEach(lightboxElement => {
-  // start listening to when they are clicked
-  lightboxElement.addEventListener('click', () => {
-    // get the big src from the lightbox element
-    const src = lightboxElement.dataset.lightboxSrc;
-    
-    // whatever you need to do to open the modal
-    openLightBox(src);
-  });
-});
+ // on each lightbox element
+ lightboxElements.forEach(lightboxElement => {
+     // start listening to when they are clicked
+     lightboxElement.addEventListener('click', () => {
+         // get the big src from the lightbox element
+         const src = lightboxElement.dataset.lightboxSrc;
 
-// checks keys pressed and runs close Modal if key is Escape
-document.addEventListener('keydown', ()=> {
-    if (event.key === 'Escape') {
-        closeModal();
-    }
-    
-});
+         openLightBox(src);
+     });
+ });
 
-function openLightBox(src) {
-  // update the src in your big image and check the selector is right here
-  document.querySelector('.modal img').src = src;
-  // makes model visible
-  document.querySelector('.modal').classList.add('visible');
-}
-// closes modal
-function closeModal() {
-  document.querySelector('.modal').classList.remove('visible');
+ // checks keys pressed and runs close Modal if key pressed is Escape
+ document.addEventListener('keydown', () => {
+     if (event.key === 'Escape') {
+         closeModal();
+     }
+
+ });
+
+ function openLightBox(src) {
+     // update the src in your big image and checks the selector is right here
+     document.querySelector('.modal img').src = src;
+     // makes model visible
+     document.querySelector('.modal').classList.add('visible');
+ }
+ // closes modal
+ function closeModal() {
+     document.querySelector('.modal').classList.remove('visible');
+ }
+
+
+var myIndex = 0;
+mySlide();
+
+function mySlide() {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(mySlide, 2000); // Change image every 2 seconds
 }
